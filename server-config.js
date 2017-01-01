4f7e551990f2aa26e2fe2bdc2d5c7b8b149bef75
Dicky Ho <me@ckho.info>
2017-01-02 01:57:46 +0800

    Initial

---
+++ server-config.js
@@ -0,0 +1,11 @@
+// Put here your email sending configurations!
+module.exports = {
+	emailTransport: {
+		// host: 'yoursmtpserver',
+		// port: 25,
+		// auth: { user: '####', pass: '####' }
+	},
+	emailOptions: {
+		from: '', // sender address
+	}
+};
